a38e8eb04c3e040d254248a24bb0a765b9fbcb6c
LM95A1 <LM95A1@gmail.com>
2023-08-08 20:21:19 -0400

    Acquired a Tuesday
    Added templates directory to house index.html
    Updated spiderweb.py since the original was f**ked
    Updated script.js for additional features
    Updated style.css to add a bunch of different options, many of which may remain unused
    Added pokeball.png in images directory; I was sick of the f**king paw logo
    
    Been experimenting with the structure of the dashboard and the features of the Pokedex to better fit what we're trying to do. Making progress at a very slow rate, given the time constraints and outside obligations piling up. Bad timing, yeah? Gonna have to go full throttle with this tonight and tomorrow.

--- spiderweb.py
+++ spiderweb.py
@@ -1,17 +1,17 @@
 # Spider Web Flask App
 # "Hell, just hope it works."
 
-from flask import Flask, send_from_directory
+from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='.')
+app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 @app.route('/static/<path:path>')
-def send_static(path):
+def serve_static(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,129 +1,79 @@
-// Function to fetch Pokemon data from a local JSON file
+// Function to fetch Pokémon data from a local JSON file
 const fetchPokemonData = () => {
-    fetch('static/data/pokemon.json')
-      .then(response => response.json())
-      .then(data => {
-        populatePokemonList(Object.values(data));
-        populatePokemonDropdown(Object.values(data));
-      })
-      .catch(error => console.error('Error fetching Pokemon data:', error));
+  fetch('static/data/pokemon.json')
+    .then(response => response.json())
+    .then(data => {
+      populatePokemonDropdown(Object.values(data), 'pokemon-dropdown-1');
+      populatePokemonDropdown(Object.values(data), 'pokemon-dropdown-2'); // Populate the second dropdown as well
+    })
+    .catch(error => console.error('Error fetching Pokémon data:', error));
+};
+
+// Function to populate the Pokémon dropdown
+const populatePokemonDropdown = (pokemons, dropdownId) => {
+  const dropdown = document.getElementById(dropdownId);
+  pokemons.forEach(pokemon => {
+    const option = document.createElement('option');
+    option.value = pokemon.name;
+    option.textContent = pokemon.name;
+    dropdown.appendChild(option);
+  });
+};
+
+const displayPokemonDetails = (pokemon, idSuffix) => {
+  document.getElementById(`pokemon-name-${idSuffix}`).textContent = pokemon.name;
+  document.getElementById(`pokemon-image-${idSuffix}`).src = pokemon.image_url;
+  const pokemonType = document.getElementById(`pokemon-type-${idSuffix}`);
+  pokemonType.src = 'path-to-type-image'; // Adjust based on how you're handling type images
+
+  // Radar Chart configuration
+  const radarData = {
+    labels: pokemon.stats.map(stat => stat.name),
+    datasets: [{
+      label: pokemon.name,
+      data: pokemon.stats.map(stat => stat.base_stat),
+      backgroundColor: 'rgba(255, 0, 0, 0.2)',
+      borderColor: 'rgba(255, 0, 0, 1)',
+      borderWidth: 3
+    }]
   };
   
-  // Function to populate the Pokemon list
-  const populatePokemonList = (pokemons) => {
-    const pokemonList = document.getElementById('pokemon-list');
-    // Iterate through the Pokemon data and create list items
-    pokemons.forEach(pokemon => {
-      const listItem = document.createElement('li');
-      listItem.textContent = pokemon.name;
-      listItem.addEventListener('click', () => displayPokemonDetails(pokemon));
-      pokemonList.appendChild(listItem);
-    });
-  };
-  
-  // Function to populate the Pokemon dropdown
-  const populatePokemonDropdown = (pokemons) => {
-    const dropdown = document.getElementById('type-dropdown'); // Adjust the ID to match your dropdown
-    pokemons.forEach(pokemon => {
-      const option = document.createElement('option');
-      option.value = pokemon.name;
-      option.textContent = pokemon.name;
-      dropdown.appendChild(option);
-    });
-  };
-  
-  // Function to filter Pokemon by name from the search bar
-  const filterPokemonByName = () => {
-    const searchBar = document.getElementById('search-bar').value.toLowerCase();
-    fetch('static/data/pokemon.json')
-      .then(response => response.json())
-      .then(data => {
-        const pokemons = Object.values(data);
-        const filteredPokemon = pokemons.find(p => p.name.toLowerCase() === searchBar);
-        if (filteredPokemon) {
-          displayPokemonDetails(filteredPokemon);
+  const radarConfig = {
+    type: 'radar',
+    data: radarData,
+    options: {
+      scales: {
+        r: {
+          beginAtZero: true
         }
-      })
-      .catch(error => console.error('Error fetching Pokemon data:', error));
+      }
+    },
   };
-  
-  // Function to display Pokemon details including the radar chart
-  const displayPokemonDetails = (pokemon) => {
-    // Display details
-    document.getElementById('pokemon-name').textContent = pokemon.name;
-    document.getElementById('pokemon-image').src = pokemon.image_url;
-    const pokemonType = document.getElementById('pokemon-type');
-    pokemonType.innerHTML = '';
-    pokemon.types.forEach(type => {
-      pokemonType.innerHTML += `${type.name} `;
-    });
-    const pokemonDescription = document.getElementById('pokemon-description');
-    pokemonDescription.innerHTML = '';
-    pokemon.stats.forEach(stat => {
-      pokemonDescription.innerHTML += `${stat.name}: ${stat.base_stat}<br>`;
-    });
-  
-    // Radar Chart configuration
-    const data = {
-      labels: ['HP', 'Attack', 'Defense', 'Special Attack', 'Special Defense', 'Speed'],
-      datasets: [{
-        label: pokemon.name,
-        data: pokemon.stats.map(stat => stat.base_stat),
-        backgroundColor: 'rgba(255, 0, 0, 0.2)',
-        borderColor: 'rgba(255, 0, 0, 1)',
-        borderWidth: 3
-      }]
-    };
-  
-    const config = {
-      type: 'radar',
-      data: data,
-      options: {
-        elements: {
-          line: {
-            borderWidth: 3
-          }
-        }
-      },
-    };
-  
-    // Update or create the chart
-    const ctx = document.getElementById('stat-chart').getContext('2d');
-    if (window.chart) {
-      window.chart.destroy(); // Destroy existing chart if there is one
-    }
-    window.chart = new Chart(ctx, config);
-  
-    // Link to Pokemon's official website
-    const pokemonLink = document.createElement('a');
-    pokemonLink.href = pokemon.pokemon_url;
-    pokemonLink.textContent = "Visit Pokémon's official website";
-    document.getElementById('pokemon-details').appendChild(pokemonLink);
-  };
-  
-  // Event listener for the dropdown selection
-  document.getElementById('type-dropdown').addEventListener('change', (event) => {
-    const selectedPokemonName = event.target.value;
-    fetch('static/data/pokemon.json')
-      .then(response => response.json())
-      .then(data => displayPokemonDetails(data[selectedPokemonName]))
-      .catch(error => console.error('Error fetching Pokemon data:', error));
-  });
-  
-  // Event listener for the search bar
-  document.getElementById('search-bar').addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-      filterPokemonByName();
-    }
-  });
-  
-  // Event listener for the reset button
-  document.getElementById('reset-button').addEventListener('click', () => {
-    // Reset the interface to its initial state
-    document.getElementById('pokemon-list').innerHTML = '';
-    fetchPokemonData();
-  });
-  
-  // Fetch the initial Pokemon data when the page loads
-  fetchPokemonData();
-  
+
+  const radarCtx = document.getElementById(`radarChart${idSuffix}`).getContext('2d');
+  if (window[`radarChart${idSuffix}`]) {
+    window[`radarChart${idSuffix}`].destroy();
+  }
+  window[`radarChart${idSuffix}`] = new Chart(radarCtx, radarConfig);
+};
+
+// Event listener for the first dropdown selection
+document.getElementById('pokemon-dropdown-1').addEventListener('change', (event) => {
+  const selectedPokemonName = event.target.value;
+  fetch('static/data/pokemon.json')
+    .then(response => response.json())
+    .then(data => displayPokemonDetails(data[selectedPokemonName], '1'))
+    .catch(error => console.error('Error fetching Pokemon data:', error));
+});
+
+// Event listener for the second dropdown selection
+document.getElementById('pokemon-dropdown-2').addEventListener('change', (event) => {
+  const selectedPokemonName = event.target.value;
+  fetch('static/data/pokemon.json')
+    .then(response => response.json())
+    .then(data => displayPokemonDetails(data[selectedPokemonName], '2'))
+    .catch(error => console.error('Error fetching Pokemon data:', error));
+});
+
+// Fetch the initial Pokemon data when the page loads
+fetchPokemonData();
